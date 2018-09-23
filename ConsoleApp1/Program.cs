c9fc0e61a926ded2f579356cd2b77ba08422bd37
kebaliva <iuk1990.2013@gmail.com>
2018-09-23 08:03:00 +0300

    ch

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chislo chislo = new Chislo();
+            chislo.Metod();
+        }
+    }
+    class Chislo
+    {
+        string i;
+        int colichestvo;
+
+        public void Metod()
+        {
+            Console.Write("Введите число: ");
+            i = Console.ReadLine();
+            int a = 0;
+
+            while (a <= i.Length)
+            {
+                colichestvo = a++;
+            }
+            Console.WriteLine("Количество цифр {0}", colichestvo);
+            Console.ReadKey();
+        }
+        }
+    }
+
